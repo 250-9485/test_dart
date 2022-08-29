5aceabf8c4a013adf30a025a5069f6fa797e1f08
Kosuke <kosuke.n.0722@gmail.com>
2022-08-30 01:49:31 +0900

    DemoをDatteに変更しました。

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Datte',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
